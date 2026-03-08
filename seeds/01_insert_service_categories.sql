d775dd335b19542045ff981685c8bebeea7fd235
Maks Shymanskiy <maxshymanskiy@gmail.com>
2026-03-08 21:17:21 +0200

    feat(db): test insert

---
+++ seeds/01_insert_service_categories.sql
@@ -0,0 +1,10 @@
+INSERT INTO service_categories (category_name) VALUES
+	('Engine'),
+	('Brakes'),
+	('Tyres'),
+	('Transmission'),
+	('Suspension'),
+	('Exhaust'),
+	('Electrical'),
+	('Bodywork'),
+	('General Maintenance');
