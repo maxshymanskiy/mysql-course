e68bee4f03c34d1cc4f300fb01148da5a6a4d815
Maks Shymanskiy <maxshymanskiy@gmail.com>
2026-03-12 18:21:44 +0200

    feat(db): insert and load mock data to db

---
+++ seeds/01_insert_positions.sql
@@ -0,0 +1,11 @@
+-- Single and multiple row insert 
+INSERT INTO positions (position_name) 
+VALUES ("Lube Technician");
+
+INSERT INTO positions (position_name) 
+VALUES 
+	("Mechanic"),
+    ("Master Mechanic"),
+    ("Collision Repair"),
+    ("Heavy Equipment"),
+    ("Manager");
