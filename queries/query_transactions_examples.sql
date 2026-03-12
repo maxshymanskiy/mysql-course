01d3bab306d71aced9cfe3fdfcf0c1e6d27b90a9
Maks Shymanskiy <maxshymanskiy@gmail.com>
2026-03-12 22:41:58 +0200

    fix: fast chagne of wrong id numbers

--- queries/query_transactions_examples.sql
+++ queries/query_transactions_examples.sql
@@ -9,9 +9,11 @@ VALUES (2, 6, 2, 'X5', 2021);
 INSERT INTO vehicles (client_id, make_id, vehicle_type_id, model, production_year)
 VALUES (3, 3, 99, 'Transit', 2019);
 
+-- Use COMMIT if you want to save whatever goes successful
 COMMIT;
 
--- Use ROLLBACK if needed
+-- Use ROLLBACK if you need to discard changes in transaction
+ROLLBACK;
 
 INSERT INTO vehicle_types (type_name) VALUES ('Camper');
 
@@ -24,9 +26,9 @@ INSERT INTO vehicles (client_id, make_id, vehicle_type_id, model, production_yea
 VALUES (2, 6, 2, 'X5', 2021);
 
 INSERT INTO vehicles (client_id, make_id, vehicle_type_id, model, production_year)
-VALUES (3, 3, 11, 'Transit', 2019);
+VALUES (3, 3, 16, 'Transit', 2019);
 
 INSERT INTO vehicles (client_id, make_id, vehicle_type_id, model, production_year)
-VALUES (4, 10, 11, 'Jumper', 2022);
+VALUES (4, 10, 16, 'Jumper', 2022);
 
 COMMIT;
